9926aa6ee97d9324419c16206605bde988f7b5b4
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-27 17:05:15 -0800

    debug handleFormSubmission function to display city, country after user submit. Co-authored by: Noah Cowan <nsncowan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@
 //PlaceList Logic
 
 function PlaceList() {
-  this.locations = {};
+  this.places = {};
   this.currentId = 0;
 }
 
-PlaceList.prototype.addLocationName = function(location) {
-  location.id = this.assignId();
-  this.locations[location.id] = location;
+PlaceList.prototype.addPlaceName = function(place) {
+  place.id = this.assignId();
+  this.places[place.id] = place;
 };
 
 PlaceList.prototype.assignId = function() {
@@ -16,9 +16,16 @@ PlaceList.prototype.assignId = function() {
   return this.currentId;
 };
 
-//Location Logic
+PlaceList.prototype.findPlace = function(id) {
+  if (this.places[id] !== undefined) {
+    return this.places[id];
+  }
+  return false;
+};
 
-function Location(city, country, year, landmark, notes) {
+//Place Logic
+
+function Place(city, country, year, landmark, notes) {
   this.city = city;
   this.country = country;
   this.year = year;
@@ -26,11 +33,42 @@ function Location(city, country, year, landmark, notes) {
   this.notes = notes;
 }
 
-Location.prototype.fullName = function() {
+Place.prototype.fullName = function() {
   return this.city + ", " + this.country;
 };
+// let testPlace = new Place("Kigali", "Rwanda", 2013, "", "");
+// let testPlace2 = new Place("Tokyo", "Japan", 2017, "", "");
 
+//UI Logic
 
+let placeList = new PlaceList();
 
-// let testLocation = new Location("Kigali", "Rwanda", 2013, "", "");
-// let testLocation2 = new Location("Tokyo", "Japan", 2017, "", "");
+function listPlaces(placeListToDisplay) {
+  let placesDiv = document.querySelector("div#places");
+  placesDiv.innerText = null;
+  const ul = document.createElement("ul");
+  Object.keys(placeListToDisplay.places).forEach(function(key) {
+    const place = placeListToDisplay.findPlace(key);
+    const li = document.createElement("li");
+    li.append(place.fullName());
+    li.setAttribute("id", place.id);
+    ul.append(li);
+  });
+  placesDiv.append(ul);
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedCity = document.querySelector("input#new-city").value;
+  const inputtedCountry = document.querySelector("input#new-country").value;
+  const inputtedYear = document.querySelector("input#new-year").value;
+  const inputtedLandmark = document.querySelector("input#new-landmark").value;
+  const inputtedNotes = document.querySelector("input#new-notes").value;
+  let newPlace = new Place(inputtedCity, inputtedCountry, inputtedYear, inputtedLandmark, inputtedNotes);
+  placeList.addPlaceName(newPlace);
+  listPlaces(placeList);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-place").addEventListener("submit", handleFormSubmission);
+});
